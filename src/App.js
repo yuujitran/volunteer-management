8887d8c9bd25d6979cfe91279fbc25b7aceb4eec
Aryan23411 <prajapatiaryann@gmail.com>
2025-06-28 18:42:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import LoginPage from './LoginPage';
 import RegisterPage from './RegisterPage';
 import ProfilePage from './ProfilePage';
 import EventManagementPage from './EventManagementPage';
+import NotificationBanner from './NotificationBanner';
 
 
 function App() {
   return (
     <Router>
+      <div style={{ padding: '1rem', fontFamily: 'Arial' }}>
+        <NotificationBanner />
+  
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
